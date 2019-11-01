82bf9649f59351ab20ed9d35477744459eab23ab
DianaSanchez1103 <00188618@uca.edu.sv>
2019-11-01 11:28:46 -0600

    Updates

---
+++ precache-manifest.f38c82d7c173fcaef7ab35052d76d1fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316c1bca14f8be6c44a87a0feb2337ba",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "22c656469bfb860a88aa",
+    "url": "/TicTacToe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/TicTacToe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "22c656469bfb860a88aa",
+    "url": "/TicTacToe/static/js/main.8a63f786.chunk.js"
+  },
+  {
+    "revision": "664d87082df4062f2db9",
+    "url": "/TicTacToe/static/js/runtime-main.326ff5c5.js"
+  }
+]);
